1187dfa3f69bce50dfe2b959da7df862d08df4c4
root <root@clfgsacsry.local>
2024-05-31 16:46:36 +0000

    update

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const express = require('express')
 const bodyParser = require('body-parser');
 const Logger = require('./lib/Logger');
+const path = require('path');
 const app = express();
 const ClientWA = require('./lib/ClientWA');
 
-const clientNeiros = new ClientWA('neiros');
-clientNeiros.init();
+const clients = {};
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, }));
@@ -16,10 +16,30 @@ app.use(Logger.request);
 app.get('/', (req, res) => res.send('<h1>The server is running</h1>'));
 
 app.get('/phone/:phone', async (req, res) => {
-    if (!clientNeiros) return res.status(500).send('Error clientNeiros');
+    if (!clients['neiros']) return res.status(500).send('Error clientNeiros');
     res.status(200);
-    const result = await clientNeiros.checkPhone(req.params.phone);
+    const result = await clients['neiros'].checkPhone(req.params.phone);
     res.send(result);
+});
+
+app.get('/init', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/init/:clientId/result', (req, res) => {
+    const clientId = req.params.clientId;
+    if (!clients[clientId]) {
+        var client = new ClientWA(clientId, { sendingMessageInfo: clientId == 'neiros' });
+        client.init();
+        clients[clientId] = client;
+    } else {
+        var client = clients[clientId];
+        if (client.qr) {
+            res.send(client.qr);
+        } else {
+            res.send('');
+        }
+    }
 })
 
 app.use(Logger.error);
